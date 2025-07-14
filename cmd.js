77c2b0c579d6cfb2193f3d160288ad966a998eef
Barbara-RochaSantos <contato.barbararochasantos@gmail.com>
2025-07-14 19:44:02 -0300

    Adicionando explicações

--- cmd.js
+++ cmd.js
@@ -1,6 +1,6 @@
 // Introduções a Funções
 // Simulação "Torradeira"
-function prepararPao (){
+function prepararPao (){                            //Primeiro crio as funções cada uma com uma "ação" para fazer uma torrada
 	console.log ("Pegando o pão no saco")
     console.log ("Passando a manteiga no pão")
 		let torradeiraLigada = true;
@@ -20,10 +20,10 @@ function injetarPao(){
 	console.log ("Finalizado")
 }
 
-function mainTorrada (){
-	prepararPao ()
+function mainTorrada (){                                 //Aqui crio a função main ou função "mãe", onde coloco todas as ações englobadas para fazer a torrada, deste modo, consigo apenas chamando a função 'main' executar todas as ações necessárias. 
+	prepararPao ()                                      //Deste modo elimino a necessidade de chamar cada uma das ações/funções isoladamente, otimizando o trabalho.
     torrar ()
     injetarPao ()
     }
     
-mainTorrada ()
+mainTorrada ()               //Aqui chamo a função 'main' e executo.
